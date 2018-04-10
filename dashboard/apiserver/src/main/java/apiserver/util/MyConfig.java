5150df32551961a8d74b46268f10967a03862576
lab401\lwh <2391289924@qq.com>
2018-04-10 17:56:32 +0800

    Complete the function of checking the service is able to serve

--- dashboard/apiserver/src/main/java/apiserver/util/MyConfig.java
+++ dashboard/apiserver/src/main/java/apiserver/util/MyConfig.java
@@ -7,8 +7,35 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix="myConfig") //接收application.yml中的myConfig下面的属性
 public class MyConfig {
 
-    private String apiServer;
+    private String masterIp;
+    private String username;
+    private String passwd;
     private String token;
+    private String apiServer;
+
+    public String getMasterIp() {
+        return masterIp;
+    }
+
+    public void setMasterIp(String masterIp) {
+        this.masterIp = masterIp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
 
     public String getApiServer() {
         return apiServer;
